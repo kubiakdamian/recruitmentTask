45bd0e63bef37acb45a9338a4964087602c00425
Damian Kubiak <kubiak.damian96@gmail.com>
2018-05-22 18:44:04 +0200

    Generating date, surname and state filling added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { generateForeName } from './dataGenerator'
+import "./style.css";
+import { generateForeName, generateDay, generateYear, generateMonth, generateBirthday, generateLastName } from './dataGenerator';
 
 class App extends Component {
+  state = {
+    foreNames: [],
+    lastNames: [],
+    birthdays: []
+  }
+
+  componentWillMount(){
+    this.fillData();
+  }
+
+  fillData = () => {
+    for(var i = 0; i < 200; i++){
+      this.state.foreNames.push(generateForeName());
+      this.state.lastNames.push(generateLastName());
+      this.state.birthdays.push(generateBirthday());
+    }
+  }
+
   render() {
     return (
-      <div className="App" style={{border: "1px solid black", height: "100px"}} onClick={generateForeName}>
+      <div className="App">
+        <table style={{width: "100%"}}>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th> 
+            <th>Birthday</th>
+          </tr>
+          
+          {this.state.foreNames.map(i =>
+            <tr>
+              <td>{i}</td>
+              <td>i</td> 
+              <td>i</td>
+            </tr>
+          )}
+        </table>
       </div>
     );
   }
--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -1,8 +1,6 @@
 export const generateForeName = () => {
     var name = ["Adam", "Adrien", "David", "John", "Bob", "Suzie", "Blanca", "Elizabeth", "Angelica", "Hannah"];
 
-    console.log("NAME", generateName(name));
-
     return generateName(name);
 }
 
@@ -10,6 +8,39 @@ export const generateLastName = () => {
     var name = ["Smith", "Jones", "Williams", "Brown", "Taylor", "Davis", "Wilson", "Evans", "Thomas", "Roberts"];
     
     return generateName(name);
+}
+
+export const generateBirthday = () => {
+    var birthday = generateMonth() + "/" + generateDay() + "/" + generateYear();
+    console.log(birthday);
+}
+
+const generateDay = () => {
+    var day = Math.floor(Math.random() * (30)) + 1;
+
+    return addZero(day);
+}
+
+const generateMonth = () => {
+    var month = Math.floor(Math.random() * (12)) + 1;
+    
+    return addZero(month);
+}
+
+const generateYear = () => {
+    return (Math.floor(Math.random() * (70)) + 1930).toString();
+}
+
+const addZero = (number) => {
+    var temp;
+
+    if(number < 10){
+        temp = "0" + number.toString();
+    }else{
+        temp = number.toString();
+    }
+
+    return temp;
 }
 
 const generateName = (name) => {
